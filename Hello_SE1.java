28ebfccfec534d7bc0034d43aa8a492bad6032bc
alea-progaming <110019094+alea-progaming@users.noreply.github.com>
2023-08-16 11:48:23 +0800

    Create Hello_SE1.java

---
+++ Hello_SE1.java
@@ -0,0 +1,7 @@
+public class Hello_SE1 {
+  public static void main(String[] args) {
+    System.out.println("Hello, Software Engineering Course!");
+    System.out.println("My name is PURUGGANAN, Mikaela!");
+    System.out.print("I am from section 3-BSCS1.");
+  }
+}
