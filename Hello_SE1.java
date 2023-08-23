009edb9f52ba12cb3d5950e91ae8bed06e38cfee
alea-progaming <mikaela.programming@gmail.com>
2023-08-23 16:11:29 +0800

    added a new file browser-support.txt

--- Hello_SE1.java
+++ Hello_SE1.java
@@ -3,5 +3,6 @@ public class Hello_SE1 {
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is PURUGGANAN, Mikaela!");
     System.out.print("I am from section 3-BSCS1.");
+    System.out.println("That is why developers are highly-paid!");
   }
 }
