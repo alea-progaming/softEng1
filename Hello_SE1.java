22de2800a3cf417ed0da1efa6cff2c54a0e95c3f
alea-progaming <mikaela.programming@gmail.com>
2023-08-23 16:54:54 +0800

    modified a file Hello_SE1

--- Hello_SE1.java
+++ Hello_SE1.java
@@ -4,5 +4,6 @@ public class Hello_SE1 {
     System.out.println("My name is PURUGGANAN, Mikaela!");
     System.out.print("I am from section 3-BSCS1.");
     System.out.println("That is why developers are highly-paid!");
+    System.out.println("Complicated world of SoftDev that is why it pays much!");
   }
 }
